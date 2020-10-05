3c8a71e74a35f134d339fa4a417aafcbb41b7b94
Danny Lin <danny@kdrag0n.dev>
2020-10-04 23:52:17 -0700

    overlay: Import product configs
    We're going to move most of our config changes here for portability.
    
    [kmou424: Rebrand to exTHmUI]
    Signed-off-by: kmou424 <me@kmou424.moe>
    Change-Id: I762071becc5d32505effeccfa3b6f13b4045b83f

--- overlay/overlay.mk
+++ overlay/overlay.mk
@@ -1,5 +1,4 @@
-# Copyright (C) 2018-2020 The LineageOS Project
-#           (C) 2020-2021 The exTHmUI Project
+# Copyright (C) 2021 The exTHmUI Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,14 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, build/target/product/aosp_arm64.mk)
-$(call inherit-product, build/target/product/gsi_release.mk)
-
-# Inherit vendor modules
-$(call inherit-product, vendor/exthm/overlay/overlay.mk)
-
-include vendor/exthm/build/target/product/exthm_generic_target.mk
-
-TARGET_NO_KERNEL_OVERRIDE := true
-
-PRODUCT_NAME := exthm_arm64
+# Vendor overlays
+PRODUCT_PACKAGE_OVERLAYS += vendor/exthm/overlay
