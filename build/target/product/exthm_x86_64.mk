9fde9643919c51d9ac144a1f5738da0504c466d0
HearnYuyuko <dingwenxuan4@gmail.com>
2021-12-08 11:55:11 +0000

    exthm: Add build target for Android Emulator SDK
    Change-Id: Ida9c14ebdcce6b10b94bdab444945d1dc74b93cf

---
+++ build/target/product/exthm_x86_64.mk
@@ -0,0 +1,26 @@
+# Copyright (C) 2018-2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, build/target/product/sdk_phone_x86_64.mk)
+
+$(call inherit-product, vendor/exthm/config/common.mk)
+
+# Enable mainline checking
+PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := relaxed
+
+# Overrides
+PRODUCT_NAME := exthm_x64
+PRODUCT_MODEL := exTHmUI Dev SDK x64
+
+PRODUCT_SDK_ADDON_SYS_IMG_SOURCE_PROP := $(LOCAL_PATH)/source.properties
